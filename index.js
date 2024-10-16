e0cc88b7760c440bb3b06a8cce498f160faedc4d
ajmal rambocus <ajmalrambocus@gmail.com>
2024-10-16 23:41:43 +0400

    start

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Hello, world!');
+
+// Toggle mobile menu visibility
+// document.getElementById('menu-toggle').addEventListener('click', function() {
+//     const menu = document.getElementById('mobile-menu');
+//     menu.classList.toggle('hidden');
+// });
